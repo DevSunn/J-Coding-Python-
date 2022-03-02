f7957a064872ca2a1b469bb4674789989dd46189
DevSunn <changsun050971@gmail.com>
2022-03-02 23:01:48 +0900

    little

--- hello.py
+++ hello.py
@@ -1,5 +1,28 @@
-print("Hello, World")
+# print("Hello, World")
 
-a = 1
+# a = 1
+# print(type(a))
 
-print(type(a))
+a = 3
+b = 4
+
+# / 몫
+# % 나머지
+# ** 제곱
+# \n 엔터   \t  탭     \\ \문자 그대로 사용
+
+# 인덱싱
+# 인덱싱에서 -는 역방향
+# a[ (이상) : (미만) : (간격) ]
+
+# number = 10
+# day = "three"
+# a = "I ate %d apples, so I was sick for %s days." % (number, day)
+# a = "aasdf asdfasdf asdf {name} asdfaasdf".format(name="이시영")
+
+# %0.4f    0은 간격 .4는 소수점 남기는 자리 수
+# .count("  ") 문자열의 갯수
+# .find()   가장 먼저 나오는 인덱스 / 없으면 -1
+# .upper -> 대문자  / .lower -> 소문자
+# .strip -> 공백 없애줌
+# .split 문자열 기준으로 자른다
